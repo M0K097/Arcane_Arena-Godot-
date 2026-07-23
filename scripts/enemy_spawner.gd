fb2b0594ecd5fbcec55ae5484645fe469b2d3b24
M0K0 <m0k097@gmx.de>
2026-07-23 15:27:42 +0200

    functional map for prototype v.2

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -12,7 +12,6 @@ func _on_timer_timeout() -> void:
 	if not activated:
 		return
 	var select = randi_range(0, enemies.size() - 1)
-	select = 1
 	var new_enemy = enemies[select].instantiate()
 	if player != null:
 		new_enemy.target = player
