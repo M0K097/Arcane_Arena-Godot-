402427ef8c525b08f197dc35c70f147b6e3009d8
M0K0 <m0k097@gmx.de>
2026-07-24 11:25:36 +0200

    improved spawning positions

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -22,17 +22,17 @@ var activated = true
 
 func _ready() -> void:
 	total_chance = gametime.wait_time
-	chance_worm = total_chance * 0.5
-	chance_skeleton = total_chance * 0.2
-	chance_slime = total_chance * 0.1
+	chance_worm = total_chance * 0.6
+	chance_skeleton = total_chance * 0.3
+	chance_slime = 0.1
 	gametime.start()
 
 func _on_timer_timeout() -> void:
 	if not activated:
 		return
 	var enemies_to_spawn = _choose_enemies()
-	_spawn_enemies(enemies_to_spawn)
-	print("bad_luck = ",bad_luck)
+	for marker in get_random_markers(bad_luck):
+		_spawn_enemies(enemies_to_spawn, marker)
 
 func _spawn_loot(drop_chance, signal_position):
 	_spawn_chest(drop_chance,signal_position)
@@ -49,7 +49,7 @@ func _spawn_chest(chance,at_position):
 
 func _spawm_potion(at_position):
 	var luck = randi_range(0,100)
-	if player.health == 1 and luck <= 40 or player.health <= 2 and luck <= 20 or luck <= 5:
+	if player.health == 1 and luck <= 30 or player.health <= 2 and luck <= 10 or luck <= 3:
 		var healing = healing_potion.instantiate()
 		healing.global_position = at_position
 		call_deferred("add_child", healing)
@@ -67,10 +67,21 @@ func _choose_enemies():
 		if dice >= chance_worm:
 			to_spawn.push_back(worm.instantiate())
 	return to_spawn
+	
+func get_random_markers(how_many):
+	var markers = get_children()
+	var random_markers: Array[Marker2D] = []
+	for turns in range(how_many):
+		var rng = randi_range(0,markers.size()-1)
+		var selected_marker = markers[rng]
+		random_markers.push_front(selected_marker)
+	return random_markers
 
-func _spawn_enemies(list_of_enemies):
+
+func _spawn_enemies(list_of_enemies,marker: Marker2D):
 	for enemy in list_of_enemies:
 		if player != null:
 			enemy.target = player
 		get_tree().current_scene.add_child(enemy)
+		enemy.global_position = marker.global_position
 		enemy.hookup(_spawn_loot)
